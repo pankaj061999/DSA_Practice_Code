026f7da2e4a37f61692543de94480bb7a804e5c6
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2023-07-11 17:24:56 +0530

    Two number sum in Array DSA

---
+++ Array/TwoSum/index.js
@@ -0,0 +1,37 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+/* Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+const A = [2, 7, 11, 15, 7];
+const sum = 9;
+
+const TwonuberSum = (Array, sum) => {
+  let result1 = [];
+  for (let i = 0; i < Array.length; i++) {
+    for (let j = i + 1; j < Array.length; j++) {
+      if (Array[i] + Array[j] === sum) {
+        result1.push(i);
+        result1.push(j);
+        // console.log(
+        //   "Print two number sum array result ==>>",
+        //   result1,
+        //   i,
+        //   j + 1
+        // );
+      }
+    }
+  }
+  console.log("Print two number sum array result ==>>", result1);
+  return result1;
+};
+
+TwonuberSum(A, sum);
